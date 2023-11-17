e22b57995f1fb3888163558887e5db0cfc8940ff
popibeatrice <popicabeatrice31@gmail.com>
2023-11-17 14:51:58 +0200

    schelet

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,7 @@
+// images
+import Image from "next/image";
+import heroImg from "./assets/landingHeroImage.png";
+
 export default function Home() {
-  return <main className="font-handwrite min-h-screen">Hello</main>;
+  return <main className="font-handwrite">Hello world</main>;
 }
