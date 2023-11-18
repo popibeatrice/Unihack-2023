f6ee75df617f4283e8fe8862dc1345a43baf5e02
DavidPav3l <david_marian@outlook.com>
2023-11-19 00:23:41 +0200

    aaaaa

--- app/dashboard/[courseId]/page.jsx
+++ app/dashboard/[courseId]/page.jsx
@@ -1,7 +1,6 @@
 import { getAuthSession } from "@/lib/auth";
+import VideoZone from "@/components/VideoZone";
 import prisma from "@/lib/db";
-
-// import YoutubeVideo from "@/components/YoutubeVideo";
 
 async function getCourse(session, courseId) {
   try {
@@ -42,20 +41,44 @@ export default async function TheChosenCourse({ params: { courseId } }) {
   const transformedString =
     course.name.charAt(0).toUpperCase() + course.name.slice(1);
   return (
-    <main className="flex flex-col items-center justify-start">
+    <main className="flex w-full flex-col items-center justify-start gap-10">
       <div className="flex flex-col items-center justify-center gap-5">
         <h1 className="text-center text-4xl font-light sm:text-5xl md:text-6xl">
           {transformedString}
         </h1>
-        <div className="flex w-full flex-row-reverse items-center justify-between gap-3">
+        <div className="flex w-full flex-row items-center justify-between gap-3 md:justify-start md:gap-5">
+          <span>{course.units.length} CHAPTERS</span>
           <div className="flex items-center justify-start gap-2">
             <div className="h-3 w-3 rounded-full bg-primary"></div>
             <span className="tracking-wide">Not completed</span>
           </div>
-          <span>{course.units.length} CHAPTERS</span>
         </div>
       </div>
-      {/* <YoutubeVideo videoLink="https://www.youtube.com/watch?v=9jU8PJ4gPVw&ab_channel=Calisthenicmovement" /> */}
+      <div className="flex w-[90%] flex-col items-center justify-center gap-10">
+        <div className="flex w-full items-center justify-center gap-10">
+          <VideoZone />
+          <div className="w-full max-w-xl self-stretch rounded-xl bg-zinc-800 p-5">
+            <h2 className="text-xl">Content</h2>
+            <div className="bg-zinc-700">
+              <ul>
+                <li>aaa</li>
+                <li>ccc</li>
+                <li>ccc</li>
+                <li>ccc</li>
+                <li>ccc</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="w-full max-w-[1192px] rounded-xl bg-zinc-800 p-5 2xl:max-w-[1384px]">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia,
+            omnis soluta? Et iste laborum labore. Labore, corrupti sequi
+            temporibus ea, velit magnam numquam nemo nihil eum quaerat in
+            ducimus rerum.
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
