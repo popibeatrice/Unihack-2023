2af67f9d7788d47eab9859574d19a54c9cb7373d
DavidPav3l <david_marian@outlook.com>
2023-11-18 21:26:41 +0200

    urasc windows asa mult

--- app/api/course/generateCourse/route.js
+++ app/api/course/generateCourse/route.js
@@ -1,3 +1,5 @@
+// /api/course/generateCourse
+
 import prisma from "@/lib/db";
 import strict_output from "@/lib/gpt";
 import {
@@ -7,81 +9,71 @@ import {
 } from "@/lib/youtube";
 import { NextResponse } from "next/server";
 
-import { getAuthSession } from "@/lib/auth";
-
 export async function POST(req) {
   try {
-    const session = await getAuthSession();
+    const { lessonId } = await req.json();
 
-    if (!session) {
-      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-    }
-
-    const { lessons } = await req.json();
-
-    console.log("Suntem in endpoint");
-
-    for (const lesson of lessons) {
-      const { name, youtubeSearchQuery, id } = lesson;
-
-      const lessonCheck = await prisma.lesson.findUnique({
-        where: {
-          id,
+    const lesson = await prisma.lesson.findUnique({
+      where: {
+        id: lessonId,
+      },
+    });
+    if (!lesson) {
+      return NextResponse.json(
+        {
+          message: "NOT OK",
         },
-      });
-      if (!lessonCheck) {
-        return NextResponse.json(
-          {
-            message: "Lesson not found",
-          },
-          { status: 404 },
-        );
-      }
-
-      const videoId = await searchYoutube(youtubeSearchQuery);
-      let transcript = await getTranscript(videoId);
-      let maxLength = 50;
-      transcript = transcript.split(" ").slice(0, maxLength).join(" ");
-
-      const output_summary = await strict_output(
-        "You are an AI capable of summarising a youtube transcript",
-        `summarise in 50 words or less and do not talk of the sponsors or anything unrelated to the main topic, also do not introduce what the summary is about.BE CONCISE AND SHORT TO THE POINT: ${transcript}`,
-        { summary: "summary of the transcript" },
+        { status: 404 },
       );
-      console.log(output_summary);
-
-      // await prisma.question.createMany({
-      //   data: questions.map((question) => {
-      //     let options = [
-      //       question.answer,
-      //       question.option1,
-      //       question.option2,
-      //       question.option3,
-      //     ];
-      //     options = options.sort(() => Math.random() - 0.5);
-      //     return {
-      //       question: question.question,
-      //       answer: question.answer,
-      //       options: JSON.stringify(options),
-      //       lessonId: id,
-      //     };
-      //   }),
-      // });
-
-      // await prisma.lesson.update({
-      //   where: { id },
-      //   data: {
-      //     videoId,
-      //     summary,
-      //   },
-      // });
     }
+    const videoId = await searchYoutube(lesson.youtubeSearchQuery);
+    let transcript = await getTranscript(videoId);
+    let maxLength = 200;
+    transcript = transcript.split(" ").slice(0, maxLength).join(" ");
+
+    const { summary } = await strict_output(
+      "You are an AI capable of summarising a youtube transcript",
+      "summarise in 100 words or less and do not talk of the sponsors or anything unrelated to the main topic, BE BRIEF, also do not introduce what the summary is about.\n" +
+        transcript,
+      { summary: "summary of the transcript" },
+    );
+
+    console.log(summary);
+
+    const questions = await getQuestionsFromTranscript(summary, lesson.name);
+
+    console.log(questions);
+
+    await prisma.question.createMany({
+      data: questions.map((question) => {
+        let options = [
+          question.answer,
+          question.option1,
+          question.option2,
+          question.option3,
+        ];
+        options = options.sort(() => Math.random() - 0.5);
+        return {
+          question: question.question,
+          correctAnswer: question.answer,
+          answers: JSON.stringify(options),
+          lessonId: lessonId,
+        };
+      }),
+    });
+
+    await prisma.lesson.update({
+      where: { id: lessonId },
+      data: {
+        youtubeVideoId: videoId,
+        summary: summary,
+      },
+    });
     return NextResponse.json({ message: "OK" }, { status: 200 });
   } catch (error) {
-    console.log(error);
     return NextResponse.json(
       {
-        messaage: "NOT OK",
+        message: "NOT OK",
       },
       { status: 500 },
     );
--- components/client/LessonCard.jsx
+++ components/client/LessonCard.jsx
@@ -1,12 +1,70 @@
 "use client";
+import { cn } from "@/lib/utils";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import React from "react";
 
-export default function ChapterCard({ lesson }) {
-  return (
-    <div
-      key={lesson.id}
-      className="mt-2 flex justify-between rounded bg-secondary px-4 py-2"
-    >
-      <h5>{lesson.name}</h5>
-    </div>
-  );
-}
+const LessonCard = React.forwardRef(
+  ({ lesson, lessonIndex, setCompletedLessons, completedLessons }, ref) => {
+    const [success, setSuccess] = React.useState(null);
+    const { mutate: getLessonInfo, isLoading } = useMutation({
+      mutationFn: async () => {
+        const response = await axios.post("/api/course/generateQuestions", {
+          lessonId: lesson.id,
+        });
+        return response.data;
+      },
+    });
+
+    const addLessonIdToSet = React.useCallback(() => {
+      setCompletedLessons((prev) => {
+        const newSet = new Set(prev);
+        newSet.add(lesson.id);
+        return newSet;
+      });
+    }, [lesson.id, setCompletedLessons]);
+
+    React.useEffect(() => {
+      if (lesson.youtubeVideoId) {
+        setSuccess(true);
+        addLessonIdToSet;
+      }
+    }, [lesson, addLessonIdToSet]);
+
+    React.useImperativeHandle(ref, () => ({
+      async triggerLoad() {
+        if (lesson.youtubeVideoId) {
+          addLessonIdToSet();
+          return;
+        }
+        getLessonInfo(undefined, {
+          onSuccess: () => {
+            setSuccess(true);
+            addLessonIdToSet();
+          },
+          onError: (error) => {
+            console.error(error);
+            setSuccess(false);
+            addLessonIdToSet();
+          },
+        });
+      },
+    }));
+    return (
+      <div
+        key={lesson.id}
+        className={cn("mt-2 flex justify-between rounded px-4 py-2", {
+          "bg-secondary": success === null,
+          "bg-red-500": success === false,
+          "bg-green-500": success === true,
+        })}
+      >
+        <h5>{lesson.name}</h5>
+      </div>
+    );
+  },
+);
+
+LessonCard.displayName = "LessonCard";
+
+export default LessonCard;
