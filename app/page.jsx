7788dcab54d3983492a1c146a470b856a3fc8d2e
popibeatrice <popicabeatrice31@gmail.com>
2023-11-19 02:54:33 +0200

    aiaiai

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,15 @@
 // images
 import Image from "next/image";
 import heroImg from "./assets/landingHeroImage.png";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { getAuthSession } from "@/lib/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getAuthSession();
   return (
     <main className="relative mx-auto grid min-h-[calc(100vh_-_175px)] w-full max-w-7xl grid-cols-1 items-center justify-items-center gap-5 px-2 sm:grid-cols-2 sm:gap-0">
-      <div className="flex flex-col items-center justify-end gap-2 self-end sm:items-start sm:justify-center sm:self-auto sm:justify-self-start">
+      <div className="flex flex-col items-center justify-end gap-3 self-end sm:items-start sm:justify-center sm:self-auto sm:justify-self-start">
         <h1 className="text-center text-xl font-extralight uppercase min-[350px]:text-2xl min-[475px]:text-3xl sm:text-start sm:text-2xl md:text-4xl xl:text-5xl 2xl:text-6xl">
           Learning made by <br className="sm:hidden" />{" "}
           <span className="font-medium">people</span>, <br />
@@ -16,6 +20,15 @@ export default function Home() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
           obcaecati?
         </p>
+        {!session ? (
+          <Link className={buttonVariants({ variant: "default" })} href="/">
+            Join now
+          </Link>
+        ) : (
+          <Link className={buttonVariants({ variant: "default" })} href="/">
+            Dashboard
+          </Link>
+        )}
       </div>
       <div className="flex w-full flex-col items-center justify-center self-start sm:self-auto">
         <Image
--- components/client/FriendRequest.jsx
+++ components/client/FriendRequest.jsx
@@ -1,27 +1,66 @@
 "use client";
-
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "../ui/button";
 
 function FriendRequest({
   children,
+  imgUrl,
   acceptFriendRequest,
   id,
   declineFriendRequest,
 }) {
   return (
-    <div className="flex items-center justify-center gap-3">
-      <Button type="button" onClick={() => acceptFriendRequest(id)}>
-        Accept
-      </Button>
-      <span>{children}</span>
-      <Button
-        type="button"
-        variant="destructive"
-        onClick={() => declineFriendRequest(id)}
-      >
-        Decline
-      </Button>
-    </div>
+    <li className="flex items-center justify-between gap-3 rounded-xl bg-zinc-700 px-4 py-2">
+      <div class="flex items-center justify-center gap-4">
+        <Avatar className="h-10 w-10 sm:h-12 sm:w-12">
+          <AvatarImage src={imgUrl} />
+          <AvatarFallback>PFP</AvatarFallback>
+        </Avatar>
+        <span className="text-xl font-medium">{children}</span>
+      </div>
+      <div className="flex items-center justify-center gap-3">
+        <Button
+          type="button"
+          className="bg-green-600 duration-300 hover:bg-green-700"
+          onClick={() => acceptFriendRequest(id)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#fff"
+            class="h-6 w-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M4.5 12.75l6 6 9-13.5"
+            />
+          </svg>
+        </Button>
+        <Button
+          type="button"
+          className="bg-red-700 duration-300 hover:bg-red-800"
+          onClick={() => declineFriendRequest(id)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#fff"
+            class="h-6 w-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+            />
+          </svg>
+        </Button>
+      </div>
+    </li>
   );
 }
 
